2b183e0b3f14bde6b130c26461365caf97456e0d
Terry Kow Zhi Im <terryim@107-144-156-227.biz.spectrum.com>
2019-07-20 18:43:22 -0400

    account tostring, getters setters from T's side

--- BankingApp/BankApp/src/main/java/com/revature/beans/Account.java
+++ BankingApp/BankApp/src/main/java/com/revature/beans/Account.java
@@ -17,6 +17,46 @@ public class Account {
 		this.isJoint=isJoint;
 		this.isApproved=isApproved;
 	}
+
+	public ArrayList<User> getUser() {
+		return User;
+	}
+
+	public void setUser(ArrayList<User> user) {
+		User = user;
+	}
+
+	public float getCurrentBalance() {
+		return CurrentBalance;
+	}
+
+	public void setCurrentBalance(float currentBalance) {
+		CurrentBalance = currentBalance;
+	}
+
+	public boolean isJoint() {
+		return isJoint;
+	}
+
+	public void setJoint(boolean isJoint) {
+		this.isJoint = isJoint;
+	}
+
+	public boolean isApproved() {
+		return isApproved;
+	}
+
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [User=" + User + ", CurrentBalance=" + CurrentBalance + ", isJoint=" + isJoint + ", isApproved="
+				+ isApproved + "]";
+	}
+	
+	
 	
 	
 }
