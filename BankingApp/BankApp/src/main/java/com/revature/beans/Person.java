97c0d232729e16c70e3fb29a1669638440faab65
Terry Kow Zhi Im <terryim@107-144-156-227.biz.spectrum.com>
2019-07-20 18:04:40 -0400

    changed gitignore.

--- BankingApp/BankApp/src/main/java/com/revature/beans/Person.java
+++ BankingApp/BankApp/src/main/java/com/revature/beans/Person.java
@@ -2,5 +2,5 @@ package com.revature.beans;
 
 public class Person {
 	
-	//person has String name, ArrayList<Account> accounts, String password, int role
+	//person has Strinsasg name, ArrayList<Account> accounts, String password, int role
 }
