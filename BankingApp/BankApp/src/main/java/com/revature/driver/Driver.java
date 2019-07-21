613acf64e37651a101d0600d2767eff6175faba9
Leoschenck <leoschenck@gmx.de>
2019-07-21 01:34:49 -0400

    added view method description in commentary. Interact optiones, but NOT what is displayed

--- BankingApp/BankApp/src/main/java/com/revature/driver/Driver.java
+++ BankingApp/BankApp/src/main/java/com/revature/driver/Driver.java
@@ -1,12 +1,29 @@
 package com.revature.driver;
 
+import java.util.ArrayList;
+
+import com.revature.beans.Account;
+import com.revature.beans.User;
 import com.revature.view.View;
 
 public class Driver {
+	static ArrayList<Account> application;
+	static ArrayList<Account> accounts;
+	static ArrayList<User> users;
+	
 	//methods to checkPassword for a user who tried to login, to withdraw,transfer,deposit (maybe 
 	//we should throw exceptions from model instead?)
 	public static void main(String[] args) {
 	//redirects into view; 	
+		System.out.println(View.factorial(5));
 		View.startView();
+		
+	}
+	public static void loginUser(String userName, String password) {
+		System.out.println("no match, sorriiyii");
+	}
+	public static void getApplicationView() {
+		//collect all applications here
+		View.applicationView(application);
 	}
 }
--- BankingApp/BankApp/src/main/java/com/revature/view/View.java
+++ BankingApp/BankApp/src/main/java/com/revature/view/View.java
@@ -1,6 +1,10 @@
 package com.revature.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import com.revature.beans.Account;
+import com.revature.driver.Driver;
 
 /*
  * Scanner = new Scanner() to reset.
@@ -15,14 +19,17 @@ public class View {
 
 	public static void startView() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to the banking bash! To log in, enter -l, or to register an account, enter -r");
+		System.out.println(
+				"Welcome to the banking bash! To log in, enter -l, or to "
+				+ "register an account, enter -r \n\n");
 
 		input = sc.nextLine();
-		if (input.length() < 2) {
+		if (input.length() < 2 || input.charAt(0) != '-') {
+			System.out.println("Choose -r or -l   (^_^)");
 			startView();
 		}
 
-		switch (input.toCharArray()[1]) {
+		switch (input.charAt(1)) {
 		case ('l'):
 			loginView();
 			break;
@@ -38,10 +45,54 @@ public class View {
 	}
 
 	public static void loginView() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("To login, enter your user name now. To cancel, enter -c");
 
+		input = sc.nextLine();
+		if (input.length() < 2) {
+			System.out.println("Usernames are longer than one character. To cancel, enter -c");
+			loginView();
+		} else if (input.equals("-c")) {
+			System.out.println("You cancelled the login and are back at the start screen.");
+			startView();
+		}
+		String userName = input;
+		System.out.println("please now gief password or cancel with -c");
+		input = sc.nextLine();
+		if (input.equals("-c")) {
+			System.out.println("You cancelled the login and are back at the start screen.");
+			startView();
+		} 
+		String password = input;
+		Driver.loginUser(userName, password);
 	}
 
 	public static void registerView() {
 
+	}
+	public static void applicationView(ArrayList<Account> accounts) {
+		for (Account account : accounts) {
+			System.out.println("Name: " + account.getUser());
+		}
+	}
+	/*
+	 * registerView
+	 * customerView  				-logout (as logoutView?) -a(ccountView)	
+	 * 		accountView				-w,d,t  -a(pply) 	-logout
+	 * employeeView					-logout -a(pplicationView) 	-c(ustomerInfo)
+	 * 		applicationView			-a(ccept)/d(eny) -c
+	 * adminView					-logout -a(pplicationView) 	-c(ustomerInfo for admins)
+	 * 		customerView(bool isAdmin) 	-c 	-e(dit: inputs would be: username as unique key 
+	 * 												to find user, then 0/1/2/3 for which 
+	 * 												variable, then new value; -c to cancel, 
+	 * 												still check for same name, length,...)
+	 * 
+	 * 
+	 */
+	
+	public static int factorial(int n) {
+		if (n==1) {
+			return 1;
+		} return n*factorial(n-1); 
 	}
 }
